8c05d15f7d37d5247498f970f4717ce06a779ef8
redberry0217 <redberry0217@gmail.com>
2024-03-19 19:56:36 +0900

    Style: header 스타일 조정

--- app/_components/layout/header.tsx
+++ app/_components/layout/header.tsx
@@ -6,20 +6,20 @@ import YellowLinkBtn from "../common/Button";
 
 const HeaderNav = () => {
   return (
-    <section className="bg-PurpleDark min-h-40 flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center p-5 gap-4">
+    <section className="bg-PurpleDark min-h-40 flex justify-end items-center">
+      <div className="flex flex-col justify-center items-center p-5 gap-4 absolute left-1/2 transform -translate-x-1/2">
         <Image src={Logo} alt="로고 이미지" width="200" />
         <nav className="flex flex-rows gap-5">
           <YellowLinkBtn href="/" text="🖌️그림그리기" />
           <YellowLinkBtn href="/" text="🎮게임하기" />
         </nav>
       </div>
-      <div className="flex justify-center items-center">
+      <div className="flex mr-10">
         <img
-          src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+          src="https://cdn-icons-png.flaticon.com/512/848/848006.png"
           alt="사용자 이미지"
-          width="40"
-          height="40"
+          width="50"
+          height="50"
         />
       </div>
     </section>
