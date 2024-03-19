9da5ab52f84b3cfac89ad8f3541c317748b0417c
HyunjinKim <koujie11@gmail.com>
2024-03-19 21:47:29 +0900

    feat : login/signup modal 기능 구현 중

--- app/_components/AuthModal/LoginModal.tsx
+++ app/_components/AuthModal/LoginModal.tsx
@@ -1,32 +1,70 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import logo from "@/public/image/logo-line.png";
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { createClient } from "@supabase/supabase-js";
 
 const LoginModal = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const router = useRouter();
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+  const handleSignIn = async () => {
+    // try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      // 로그인 요청
+      email,
+      password,
+    });
+    //   if (error) throw error;
+    //   router.push("/"); // 성공 시 홈페이지로 이동
+    // } catch (error) {
+    //   console.error("로그인 에러:", error.message);
+    // }
+  };
+
+  // 로그아웃 기능
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+  };
+
   return (
-    <section className="position : absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+    <section className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
       <div className="modal, flex flex-col justify-center items-center bg-Background w-96 h-[500px]">
-        <Image src={logo as StaticImageData} alt="로고이미지" />
+        <Link href="/">
+          <Image src={logo as StaticImageData} alt="로고이미지" />
+        </Link>
         <h2>로그인</h2>
         <br />
+
         <input
           type="email"
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
-          placeholder="이메일"
+          value="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="이메일(123@gmail.com)을 입력하세요."
         />
         <br />
         <input
           type="password"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
-          placeholder="비밀번호"
+          value="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="비밀번호를 입력하세요."
         />
         <br />
         <button>Google Login</button>
+        <br />
         <button>Kakao Login</button>
         <br />
-        <button>로그인하기</button>
+        <button type="button" onClick={handleSignIn}>
+          로그인하기
+        </button>
         <br />
         <div>
           아직 회원이 아니신가요? <button>회원가입</button>
--- app/_components/AuthModal/SignupModal.tsx
+++ app/_components/AuthModal/SignupModal.tsx
@@ -1,46 +1,89 @@
+"use client";
 import Image, { StaticImageData } from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import logo from "@/public/image/logo-line.png";
+import Link from "next/link";
+import { supabase } from "@/app/_utils/supabase/supabase";
 
 const SignupModal = () => {
-  // const handleOnClickMoveToLogin =
+  const [email, setEmail] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+
+  const handleOnSubmitSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!email || !nickname || !password || !passwordCheck) {
+      alert("모든 값를 입력해주세요.");
+      return;
+    }
+
+    if (password !== passwordCheck) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+      console.log(data);
+      if (error) {
+        alert(error.message);
+      } else {
+        // 회원가입 성공 시 로직
+      }
+    } catch (error) {
+      alert("회원가입 중 오류가 발생했습니다.");
+    }
+  };
+
   return (
     <section className="position : absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
       <div className="modal, flex flex-col justify-center items-center bg-Background w-96 h-[500px]">
-        <Image src={logo as StaticImageData} alt="로고이미지" />
+        <Link href="/">
+          <Image src={logo as StaticImageData} alt="로고이미지" />
+        </Link>
         <h2>회원가입</h2>
+        <form onSubmit={handleOnSubmitSignUp}>
+          <input
+            type="email"
+            name="email"
+            placeholder="이메일"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            name="nickname"
+            placeholder="닉네임"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="비밀번호 확인"
+            value={passwordCheck}
+            onChange={(e) => setPasswordCheck(e.target.value)}
+          />
+          <br />
+          <button className="border-solid border-2" type="submit">
+            회원가입 하기
+          </button>
+        </form>
 
-        <input
-          type="email"
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
-          placeholder="이메일"
-        />
-        <br />
-        <input
-          type="text"
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
-          placeholder="닉네임"
-        />
-        <br />
-        <input
-          type="password"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
-          placeholder="비밀번호"
-        />
-        <br />
-        <input
-          type="password"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
-          placeholder="비밀번호 확인"
-        />
-        <br />
-
-        <button className="border-solid border-2">회원가입 하기</button>
-        <br />
         <div>
           이미 회원이신가요? <button>로그인</button>
         </div>
