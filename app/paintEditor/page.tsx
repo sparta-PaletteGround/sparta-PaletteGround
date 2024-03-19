6e88645690ffab90db30b1fcf2727947b197cb45
redberry0217 <redberry0217@gmail.com>
2024-03-19 14:56:51 +0900

    Chore: painterEditor UI 수정중

--- app/paintEditor/page.tsx
+++ app/paintEditor/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PaintEditorPage = () => {
   return (
-    <section className="bg-PurpleLight text-YellowDark flex flex-col justify-center items-center">
+    <section className="bg-PurpleLightflex flex-col justify-center items-center">
       <h1>오늘의 주제</h1>
       <div>ddd</div>
     </section>
