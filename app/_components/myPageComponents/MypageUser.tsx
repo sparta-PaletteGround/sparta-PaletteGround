ab94c7e74e8ac616d2a0b5a700baa5513c227ed6
CodHan <qpscm662@naver.com>
2024-03-21 05:12:45 +0900

    [fix] 새로고침 버그 해결

--- app/_components/myPageComponents/MypageUser.tsx
+++ app/_components/myPageComponents/MypageUser.tsx
@@ -1,12 +1,17 @@
 'use client';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-import { useQuery } from '@tanstack/react-query';
-import { getUser } from './myPageSupabase';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { getUser, updateUser } from './myPageSupabase';
 import Modal from './modals/Modal';
+import { supabase } from '@/app/_utils/supabase/supabase';
+import MypageNonAuth from './MypageNonAuth';
 
 const MypageUser = () => {
   const [isOpenMidal, setIsOpenModal] = useState(false);
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [loginUserEmail, setLoginUserEmail] = useState('dkdk@naver.com');
 
   const fileInputRef: React.MutableRefObject<any> = useRef(null);
 
@@ -15,15 +20,40 @@ const MypageUser = () => {
 
   const { data, isLoading }: { data: any | null | undefined; isLoading: any } =
     useQuery({
-      queryKey: ['data'],
-      queryFn: getUser,
+      queryKey: ['user'],
+      queryFn: () => getUser({ email: loginUserEmail }),
     });
+  useEffect(() => {
+    const loginUser = async () => {
+      const { userInfo }: any = await supabase.auth.getUser();
+      if (userInfo) {
+        console.log('로그인한 유저의 정보', data.user);
+        //로그인한 유저email state에 전달
+        // setLoginUserEmail()
+        //전달 한 email queryFn에 매개변수로 전달
+        // loginUserEmail
+        return userInfo;
+      } else {
+        setIsLogin(true);
+      }
+    };
+    loginUser();
+  }, [data]);
+
+  const queryClient = useQueryClient();
+
+  const updateMutate = useMutation({
+    mutationFn: updateUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+    },
+  });
 
   if (isLoading) {
     return <div>로딩중 ... </div>;
   }
   const userInfo = data[0];
-  const { nickname, profile_img } = userInfo;
+  const { nickname, profile_img, email } = userInfo;
 
   const handleNickName = (e: any) => {
     setUpdateNickName(e.target.value);
@@ -45,83 +75,108 @@ const MypageUser = () => {
 
   const handleUpdateSubmit = () => {
     if (!updateNickName && !updateImage) {
-      alert('변경 사항이 없습니다');
-      setIsOpenModal(false);
+      return alert('변경 사항이 없습니다'), setIsOpenModal(false);
     }
     if (!updateImage) {
       console.log('닉네임만 바뀜');
+      const updateData = {
+        nickname: updateNickName,
+        email,
+      };
+      updateMutate.mutate(updateData);
     }
     if (!updateNickName) {
       console.log('이미지만 바뀜');
+      const updateData = {
+        profile_img: updateImage,
+        email,
+      };
+      updateMutate.mutate(updateData);
+    }
+    if (updateNickName && updateImage) {
+      console.log('둘다 바뀜!');
+      const updateData = {
+        nickname: updateNickName,
+        profile_img: updateImage,
+        email,
+      };
+      updateMutate.mutate(updateData);
     }
   };
 
   return (
     <>
-      <section className="border-2 bg-PurpleLight rounded-lg w-96 h-96 ml-20 mt-4">
-        <div>
-          <div className=" m-8 flex justify-center">
-            <img
-              src={profile_img}
-              alt="프로필 이미지"
-              className="w-40 h-40 rounded-full"
-            />
-          </div>
-          <div className="flex flex-col gap-5 items-center">
-            <div className="text-xl font-bold text-PurpleMedium">
-              {nickname} 페인터
-            </div>
+      {isLogin ? (
+        <>
+          <h1 className="mt-24 ml-20 text-2xl font-bold">💘 마이페이지</h1>
+          <section className="border-2 bg-PurpleLight rounded-lg w-96 h-96 ml-20 mt-4">
             <div>
-              <button
-                className="w-40 h-10 border-2 rounded-xl bg-PurpleDark text-PurplePale font-bold"
-                onClick={() => setIsOpenModal(true)}
-              >
-                정보 수정하기
-              </button>
+              <div className=" m-8 flex justify-center">
+                <img
+                  src={profile_img}
+                  alt="프로필 이미지"
+                  className="w-40 h-40 rounded-full"
+                />
+              </div>
+              <div className="flex flex-col gap-5 items-center">
+                <div className="text-xl font-bold text-PurpleMedium">
+                  {nickname} 페인터
+                </div>
+                <div>
+                  <button
+                    className="w-40 h-10 border-2 rounded-xl bg-PurpleDark text-PurplePale font-bold"
+                    onClick={() => setIsOpenModal(true)}
+                  >
+                    정보 수정하기
+                  </button>
+                </div>
+                <div className="flex gap-28 text-lg">
+                  <span>PT. 3600</span>
+                  <span>LV. 3</span>
+                </div>
+              </div>
             </div>
-            <div className="flex gap-28 text-lg">
-              <span>PT. 3600</span>
-              <span>LV. 3</span>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* 모달 섹션 */}
-      <Modal isVisible={isOpenMidal} onClose={() => setIsOpenModal(false)}>
-        <div>
-          <div className=" m-8 flex justify-center">
-            <img
-              src={updateImage ? updateImage : profile_img}
-              alt="프로필 이미지"
-              className="w-40 h-40 rounded-full cursor-pointer"
-              onClick={() => fileInputRef.current.click()}
-            />
-          </div>
-          <div className="flex flex-col gap-5 items-center">
-            <input
-              className="text-xl font-bold text-PurpleMedium rounded-lg p-2"
-              defaultValue={nickname}
-              onChange={handleNickName}
-            />
-            {/* 이미지  클릭 */}
-            <input
-              type="file"
-              accept="image/*"
-              ref={fileInputRef}
-              style={{ display: 'none' }}
-              onChange={handleFileChange}
-            />
+          </section>
+          {/* //모달섹션 */}
+          <Modal isVisible={isOpenMidal} onClose={() => setIsOpenModal(false)}>
             <div>
-              <button
-                className="w-40 h-10 border-2 rounded-xl bg-PurpleDark text-PurplePale font-bold"
-                onClick={handleUpdateSubmit}
-              >
-                수정
-              </button>
+              <div className=" m-8 flex justify-center">
+                <img
+                  src={updateImage ? updateImage : profile_img}
+                  alt="프로필 이미지"
+                  className="w-40 h-40 rounded-full cursor-pointer"
+                  onClick={() => fileInputRef.current.click()}
+                />
+              </div>
+              <div className="flex flex-col gap-5 items-center">
+                <input
+                  className="text-xl font-bold text-PurpleMedium rounded-lg p-2"
+                  defaultValue={nickname}
+                  onChange={handleNickName}
+                />
+                {/* 이미지  클릭 */}
+                <input
+                  type="file"
+                  accept="image/*"
+                  ref={fileInputRef}
+                  style={{ display: 'none' }}
+                  onChange={handleFileChange}
+                />
+                <div>
+                  <button
+                    className="w-40 h-10 border-2 rounded-xl bg-PurpleDark text-PurplePale font-bold"
+                    onClick={handleUpdateSubmit}
+                  >
+                    수정
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </Modal>
+          </Modal>
+        </>
+      ) : (
+        <MypageNonAuth />
+      )}
     </>
   );
 };
--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -4,7 +4,6 @@ import MypageUser from '../_components/myPageComponents/MypageUser';
 const MyPage = () => {
   return (
     <>
-      <h1 className="mt-24 ml-20 text-2xl font-bold">💘 마이페이지</h1>
       <MypageUser />
     </>
   );
