fd2e17e6a011f464292b559c3c4088914061d163
innes-k <innes.kwak@gmail.com>
2024-03-21 20:31:01 +0900

    [chore/#29] likes - 로직 변경전 api 삭제

--- app/_components/detail-api/likes-api.ts
+++ app/_components/detail-api/likes-api.ts
@@ -1,48 +1,5 @@
 import { supabase } from "@/app/_utils/supabase/supabase";
 
-// 좋아요 클릭시 로그인한 user의 likes_array에 그림id 추가하기
-export const insertDrawingId = async (email: string, drawingId: number) => {
-  // email을 사용하여 해당 사용자 찾기 - users 반환값은 배열 안의 객체
-  const { data: users, error } = await supabase
-    .from("users")
-    .select()
-    .eq("email", email);
-
-  if (error) {
-    console.error("Error fetching user:", error.message);
-    return;
-  }
-
-  // 해당 사용자의 likes_array에 drawingId 추가하기
-  const selectedUser = users[0];
-  const updatedBookmarksArray = [...selectedUser.likes_array, drawingId];
-
-  // 수정한 배열을 해당 유저의 likes_array에 업데이트하기
-  const { error: updateError } = await supabase
-    .from("users")
-    .update({ likes_array: updatedBookmarksArray })
-    .eq("email", email);
-
-  if (updateError) {
-    console.error("Error updating user drawings array:", updateError.message);
-    return;
-  }
-};
-
-// 유저의 likes_array 가져오기
-export const getLikesArray = async (email: string) => {
-  console.log("email", email);
-  const { data, error } = await supabase
-    .from("users")
-    .select("likes_array")
-    .eq("email", email);
-  if (error) {
-    console.error(error);
-  }
-
-  return data;
-};
-
 // likes에서 email, drawing_id 존재하는지 확인 - 유저가 좋아요 했는지 확인하기
 export const isCheckLikeState = async (drawingId: number) => {
   // current loggedIn user의 email 가져오기
