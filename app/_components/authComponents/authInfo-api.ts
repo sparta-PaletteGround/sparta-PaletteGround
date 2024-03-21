50f4b08919c66b351a30e9905263daaa7669027e
HyunjinKim <koujie11@gmail.com>
2024-03-21 13:10:50 +0900

    [feat/#20] : auth에서 로그인한 유저 정보 불러오기

--- app/_components/authComponents/authInfo-api.ts
+++ app/_components/authComponents/authInfo-api.ts
@@ -3,11 +3,11 @@ import { supabase } from "@/app/_utils/supabase/supabase";
 // auth에서 유저 정보 가져오기
 export const getLoginUserInfo = async () => {
   const { data } = await supabase.auth.getUser();
-  if (data) {
-    console.log("로그인한 유저의 정보", data.user);
-    return data;
+  if (data !== null) {
+    // console.log("로그인한 유저의 정보", data.user);
+    return data.user;
   } else {
-    console.log("현재 로그인된 사용자가 없습니다.");
+    // console.log("현재 로그인된 사용자가 없습니다.");
     return null;
   }
 };
