73767227b2cefdd3403530337ccf07a7b9b7afb2
HyunjinKim <koujie11@gmail.com>
2024-03-21 18:50:10 +0900

    [feat/#23] : detail 페이지로 id 값 넘겨주기

--- app/_components/detail-list/DrawingPosts.tsx
+++ app/_components/detail-list/DrawingPosts.tsx
@@ -1,17 +1,22 @@
 import { Posts } from "@/app/_types/detail1/posts";
+import Link from "next/link";
 
 interface PostsProps {
   post: Posts;
 }
+
 const DrawingPosts = ({ post }: PostsProps) => {
   return (
-    <li className="bg-white w-1/3 p-4 m-4 rounded-2xl flex flex-col items-center">
-      <img
-        src={`https://pmduqgivaolwydqssren.supabase.co/storage/v1/object/public/drawings/${post.drawing_url}`}
-        alt=""
-      />
-      <div>{post.title}</div>
-    </li>
+    <Link href={`/detail/${post.drawing_id}`}>
+      <li className="bg-white  w-[calc(33.33% - 16px)] h-[300px] p-4 m-4 rounded-2xl flex flex-col items-center justify-center">
+        <img
+          src={`https://pmduqgivaolwydqssren.supabase.co/storage/v1/object/public/drawings/${post.drawing_url}`}
+          alt=""
+          className="w-[300px] h-[200px]"
+        />
+        <div className="text-xLarge p-4 font-medium ">{post.title}</div>
+      </li>
+    </Link>
   );
 };
 
