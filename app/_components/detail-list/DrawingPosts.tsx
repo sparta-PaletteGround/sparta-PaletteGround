893b83c7f57eecaa75ffd5870fdbefa70ddb9db6
HyunjinKim <koujie11@gmail.com>
2024-03-21 19:14:31 +0900

    [refact/#23] : 그림 클릭 시 detail 페이지로 이동

--- app/_components/detail-list/DrawingPosts.tsx
+++ app/_components/detail-list/DrawingPosts.tsx
@@ -7,16 +7,16 @@ interface PostsProps {
 
 const DrawingPosts = ({ post }: PostsProps) => {
   return (
-    <Link href={`/detail/${post.drawing_id}`}>
-      <li className="bg-white  w-[calc(33.33% - 16px)] h-[300px] p-4 m-4 rounded-2xl flex flex-col items-center justify-center">
+    <li className="bg-white  w-[calc(33.33% - 16px)] h-[300px] p-4 m-4 rounded-2xl flex flex-col items-center justify-center">
+      <Link href={`/detail/${post.drawing_id}`}>
         <img
           src={`https://pmduqgivaolwydqssren.supabase.co/storage/v1/object/public/drawings/${post.drawing_url}`}
           alt=""
           className="w-[300px] h-[200px]"
         />
-        <div className="text-xLarge p-4 font-medium ">{post.title}</div>
-      </li>
-    </Link>
+      </Link>
+      <div className="text-xLarge p-4 font-medium ">{post.title}</div>
+    </li>
   );
 };
 
